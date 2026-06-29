c5651c2854d75ea95619a41689ed9a0c51abbc2d
nour eldein <noureldein1100@gmail.com>
2026-06-29 19:38:25 +0300

    deploy fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    tsconfigPaths(),
     tanstackStart({
       server: {
         entry: "server",
