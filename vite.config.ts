7ea6ef98aafbc97c2352e481caf83f62c8afbb94
nour eldein <noureldein1100@gmail.com>
2026-06-30 01:09:33 +0300

    last f

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import nitro from "nitro/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: {
